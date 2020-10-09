bd9b2f0da58602773fa23c822832f01507bd1cc0
Alcogu <alexander092@hotmail.com>
2020-10-08 23:25:06 -0500

    guardado

---
+++ 08-Funciones/curiosidades.py
@@ -0,0 +1,17 @@
+#las funciones al principio
+#se recomienda que la funcion devuelva un dato
+
+def miFuncion():
+    return "Hola que tal"
+
+def miSegundaFuncion():
+    return "Hola que tal 2"
+
+nombre = "Alexander"
+apellidos = "Correa Gutiérrez"
+
+print("Hola Mundo")
+print(f"Bienvenido {nombre}")
+
+print(miFuncion())
+print(miSegundaFuncion())
---
+++ 08-Funciones/variables.py
@@ -0,0 +1,6 @@
+"""
+Variable local = Dentro de la funcion
+Variable global = dentro y fuera de la funcion
+"""
+
+#varibale global
