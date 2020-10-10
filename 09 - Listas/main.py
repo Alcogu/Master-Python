3ac828c2469168ffb08809f65e7b392a6be3706f
Alcogu <alexander092@hotmail.com>
2020-10-09 22:30:41 -0500

    practica hoy

---
+++ 09 - Listas/main.py
@@ -0,0 +1,22 @@
+"""
+Listas: colecciones o conjuntos de datos bajo un unico nombre
+para acceder a ess valores podemos usar un indice numerico
+"""
+
+pelicula = "Batman"
+
+#Definir lista
+peliculas = ["Spiderman", "Lord of the Ring", "Harry Potter"]
+cantantes = list(("pepito perez" , "Juanito", "pepito"))
+years = list(range(2020, 2050))
+
+"""
+print(peliculas)
+print(cantantes)
+print(years)
+"""
+#Indices
+
+print(peliculas[0])
+print(cantantes[0:2])
+print(peliculas[1:])
