d0740a626af637edfab8f3ddf9abc1816618a73d
Alcogu <alexander092@hotmail.com>
2020-11-10 23:31:00 -0500

    archivos mas y mas

--- 14-Archivos/ficheros.py
+++ 14-Archivos/ficheros.py
@@ -1,6 +1,23 @@
 from io import open
 import pathlib
 
-# Abrir Archivo
 ruta = str(pathlib.Path().absolute()) + "/ficheros.txt"
 archivo = open (ruta, "a+")
+
+#Escribir
+
+archivo.write("*************** Soy un texto metido desde Python***************\n")
+
+#Cerrar archivo
+archivo.close()
+
+# Abrir Archivo
+ruta = str(pathlib.Path().absolute()) + "/ficheros.txt"
+archivo_lectura = open (ruta, "r+")
+
+#Leer contenido
+
+contenido = archivo_lectura.read()
+
+for elemento in contenido:
+    print(elemento)
