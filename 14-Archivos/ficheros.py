05d26dc633b75af9abff26ad11bea2de0464bd45
Alcogu <alexander092@hotmail.com>
2020-11-10 23:57:47 -0500

    mas ficheros

--- 14-Archivos/ficheros.py
+++ 14-Archivos/ficheros.py
@@ -1,5 +1,6 @@
 from io import open
 import pathlib
+import shutil #acceder a fuciones de copiar y renombrar archivos
 
 ruta = str(pathlib.Path().absolute()) + "/ficheros.txt"
 archivo = open (ruta, "a+")
@@ -17,7 +18,31 @@ archivo_lectura = open (ruta, "r+")
 
 #Leer contenido
 
-contenido = archivo_lectura.read()
+#contenido = archivo_lectura.read()
+#print(contenido)
 
-for elemento in contenido:
-    print(elemento)
+#Leer contenido y guardarlo en lista
+
+lista = archivo_lectura.readlines()
+archivo_lectura.close()
+
+for frase in lista:
+
+    lista_frase = frase.split()
+    print(lista_frase)
+    #print("- " + frase.upper())
+
+#copiar
+"""
+ruta_original = str(pathlib.Path().absolute()) + "/ficheros.txt"
+ruta_nueva = str(pathlib.Path().absolute()) + "/fichero_copiado.txt"
+ruta_alternativa = "../07-ejercicios/ficheros_copiado77.txt"
+
+shutil.copyfile(ruta_original, ruta_alternativa)
+"""
+#Mover
+
+ruta_original = str(pathlib.Path().absolute()) + "/ficheros_copiado.txt"
+ruta_nueva = str(pathlib.Path().absolute()) + "/ficheros_NewCopy.txt"
+
+shutil.move(ruta_original, ruta_nueva)
