f52280afcf7444af4ae9a1f21c4faed58fd04143
Alcogu <alexander092@hotmail.com>
2020-11-11 23:39:03 -0500

    terminamos captura de errores

--- 15-Manejo de errores/main.py
+++ 15-Manejo de errores/main.py
@@ -20,6 +20,8 @@ finally:
 """
 
 #Multiples excepciones
+
+"""
 try:
     numero = int(input("Digita un numero para elevarlo al cuadrado "))
     print("El cuadrado es: " +str (numero * numero))
@@ -28,3 +30,22 @@ except TypeError:
     print("Debes convertir las cadenas a enteros")
 except ValueError:
     print("Introduce un valor conrrecto(Numerico)")
+"""
+
+#Excepsiones personalizadas
+try:
+    nombre = input("Ingresa nombre ")
+    edad = int(input("Ingresa la edad "))
+
+    if edad < 5 or edad >110:
+        raise ValueError ("La edad introducida no es real")
+    elif len(nombre) <= 1:
+        raise ValueError ("EL nombre no es correcto")
+    else:
+        print(f"Bienvenido al Master en Python {nombre}")
+
+except ValueError:
+    print("Ingresa los datos correctos")
+
+except Exception as e:
+    print("Existe un error: ", e)
