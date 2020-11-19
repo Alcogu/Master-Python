131d218ee7d2904032d2b719a6e46e6f025d2c6e
Alcogu <alexander092@hotmail.com>
2020-11-18 19:01:41 -0500

    se sube

--- 22-Django/AprendiendoDjango/miapp/views.py
+++ 22-Django/AprendiendoDjango/miapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,HttpResponse
 
-#MVC = Modelo vista controlador -> Acciones(metodos)
-#MVT = modelo vista template-> Acciones(Metodos)
+#MVC = Modelo vista controlador ->  Acciones(metodos)
+#MVT = modelo vista template->      Acciones(Metodos)
 
 def holaMundo(request):
     return HttpResponse("Hola Mundo con Django")
