aa2dc8efc22c9dc8e5956aabf267b434f2b29ea9
Alexander Correa Gutiérrez <30854317+Alcogu@users.noreply.github.com>
2020-12-02 18:06:46 -0500

    pequeñas modificaciones

--- 19-BasesdeDatos/main.py
+++ 19-BasesdeDatos/main.py
@@ -6,7 +6,7 @@ database = mysql.connector.connect(#Metodo
     host = "localhost",
     user = "root",
     passwd = "",
-    database = "master_python"
+    #database = "master_python"
 )
 
 #¿cómo saber si la conexión fue exitosa?
@@ -15,7 +15,6 @@ database = mysql.connector.connect(#Metodo
 cursor = database.cursor()#hacer consultar por el cursor
 
 cursor.execute("CREATE DATABASE IF NOT EXISTS master_python")
-
 cursor.execute("SHOW DATABASES")
 
 for bd in cursor:#COmprobar bases de datos creadas
