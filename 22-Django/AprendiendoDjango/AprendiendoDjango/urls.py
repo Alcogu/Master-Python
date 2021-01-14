762214585fae78a973bb4df5b6bc7c32cd6d2eb0
Alexander Correa Gutiérrez <30854317+Alcogu@users.noreply.github.com>
2021-01-14 13:55:34 -0500

    cambios en urls

--- 22-Django/AprendiendoDjango/AprendiendoDjango/urls.py
+++ 22-Django/AprendiendoDjango/AprendiendoDjango/urls.py
@@ -22,12 +22,12 @@ from miapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="Index"),
-    path('inicio/', views.index, name="Inicio"),
-    path('hola-mundo/', views.hola_mundo, name="Hola Mundo"),
-    path('pagina-pruebas/', views.pagina, name="Pagina"),
-    path('pagina-pruebas/<int:redirigir>',views.pagina, name="Pagina"),
-    path('contacto/', views.contacto, name="Contacto"),
-    path('contacto/<str:nombre/', views.contacto, name="Contacto"),
-    path('contacto/<str:nombre>/<str:apellidos>', views.contacto, name="Contacto")
+    path('', views.index, name="index"),
+    path('inicio/', views.index, name="inicio"),
+    path('hola-mundo/', views.hola_mundo, name="hola_mundo"),
+    path('pagina-pruebas/', views.pagina, name="pagina"),
+    path('pagina-pruebas/<int:redirigir>',views.pagina, name="pagina"),
+    path('contacto/', views.contacto, name="contacto"),
+    path('contacto/<str:nombre/', views.contacto, name="contacto"),
+    path('contacto/<str:nombre>/<str:apellidos>', views.contacto, name="contacto")
 ]
